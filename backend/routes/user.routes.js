3b10594c72f7d74eab241591662b193ed71a09a5
AnuRaw <anuragrawat230@gmail.com>
2024-09-11 19:44:02 +0530

    userroutes and addcontroller

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,16 +1,16 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { followUnfollowUser, getUserProfile } from "../controllers/user.controller.js";
+import { followUnfollowUser, getSuggestUsers, getUserProfile, updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username",protectRoute,getUserProfile);
 
-// router.get("/:suggested",protectRoute,getUserProfile);
+router.get("/suggested",protectRoute,getSuggestUsers);
 
  router.post("/follow/:id",protectRoute,followUnfollowUser);
 
-// router.post("/update",protectRoute,updateUserProfile);
+router.post("/update",protectRoute,updateUser);
 
 
 
