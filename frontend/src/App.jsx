5f22c96ed0a524362e68c821ea162ec8b81da414
AnuRaw <anuragrawat230@gmail.com>
2024-09-23 14:34:07 +0530

    2nd UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { Toaster } from 'react-hot-toast'
+import Sidebar from './components/common/SideBar.jsx'
+import RightPanel from './components/common/RightPanel.jsx'
+import LoginPage from './pages/auth/login/LoginPage.jsx'
+import SignUpPage from './pages/auth/signup/SignUpPage.jsx'
+import HomePage from './pages/home/HomePage.jsx'
+import NotificationPage from './pages/notifications/NotificationPage.jsx'
+import ProfilePage from './pages/profile/ProfilePage.jsx'  
+
+import './index.css'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className='flex max-w-6xl mx-auto'>
+        {/* Common component, bc it's not wrapped with Routes */}
+        {/* {authUser && } */}
+        <Sidebar/>
+        <Routes>
+          <Route path='/' element={ <HomePage/>}/>
+          <Route path='/login' element={ <LoginPage /> }/>
+          <Route path='/signup' element={ <SignUpPage /> } />
+           <Route path='/notifications' element={<NotificationPage/>} />
+           <Route path='/profile/:username' element={ <ProfilePage />} /> 
+        </Routes>
+        <RightPanel/>
+        {/* {authUser && } */}
+        {/* <Toaster /> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   )
 }
 
