74ee0a2b276e95cee0e8d4cb2798d6c2ceb552d4
AnuRaw <anuragrawat230@gmail.com>
2024-11-25 03:35:55 +0530

    ,

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,33 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import {v2 as cloudinary} from "cloudinary";
 import cookieParser from "cookie-parser";
-import { v2 as cloudinary } from "cloudinary";
+import authRoutes from "./routes/auth.routes.js";
+import usersRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
+import connectMongoDB from "./DB/connectMongoDB.js";
 
-import authRoutes from "./routes/auth.route.js";
-import userRoutes from "./routes/user.route.js";
-import postRoutes from "./routes/post.route.js";
-import notificationRoutes from "./routes/notification.route.js";
-
-import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
-
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
-
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
-app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
-
+ app.use(express.urlencoded({ extended: true }));
+  // to parse from data(urlencoded)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/users", usersRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
@@ -43,6 +40,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-	connectMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
 });
