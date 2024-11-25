e7cb0c404384153767a91e1bba9a8449b53e6368
AnuRaw <anuragrawat230@gmail.com>
2024-11-25 13:00:32 +0530

    .

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -193,7 +193,7 @@ const Post = ({ post }) => {
 												</div>
 												<div className='flex flex-col'>
 													<div className='flex items-center gap-1'>
-														<span className='font-bold'>{comment.user.fullName}</span>
+														<span className='font-bold'>{comment.user?.fullName}</span>
 														<span className='text-gray-700 text-sm'>
 															@{comment.user.username}
 														</span>
